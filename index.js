ae3d58b00ed153eb4c79e2af31fea60490421582
SiarhiejKresik <siarhei.kresik@gmail.com>
2019-01-06 14:47:25 +0300

    Log mongodb url

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const PlayerModel = require('./models/player');
 
 const URL = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/game2018q3';
+console.log('mongo URL:', URL)
 
 mongoose.connect(
   URL,
